ad93b49b339ec69c45c6ad4206129ab0425284aa
Orest I <orest58008@yamadachi.quest>
2024-11-18 23:07:15 +0200

    Remove .gitignore
    Signed-off-by: Orest I <orest58008@yamadachi.quest>

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import * as lexer from "./lexer.ts"
 import * as parser from "./parser.ts"
 
 const lexer_out =
-   lexer.parseByLines(Deno.readTextFileSync("./README.org").split(/\n/))
+   lexer.parseByLines(Deno.readTextFileSync("./TEST.org").split(/\n/))
 
 const parser_out =
    parser.parseByElement(lexer_out)
